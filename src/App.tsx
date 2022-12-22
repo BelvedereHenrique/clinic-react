101aaacbb3427b328403a3216bac42336c5e6499
Bruno Belvedere <belvedere.henrique@gmail.com>
2022-12-22 00:10:30 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>React App</h1>
+      <p>React App</p>
+    </div>
+  );
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  target: "web",
+  devServer: {
+    port: 3000,
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
